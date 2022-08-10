671ed8f2acf37b6533dd4eee521b2b17ffecf932
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-10 11:27:44 -0300

    Introdução ao C

--- Introducao_ao_C/exercicio_fixacao_1/main.c
+++ Introducao_ao_C/exercicio_fixacao_1/main.c
@@ -28,5 +28,20 @@ int main(void) {
 
     printf("IMC = %.2f\n", imc);
     
+    //contribuição do Dereck (classificação):
+        if(imc < 18.5){
+        printf("abaixo do peso\n");
+    }else if( imc < 24.9){
+        printf("peso normal\n");
+    }else if(imc < 29.9){
+        printf("excesso de peso\n");
+    }else if(imc < 34.9){
+         printf("obesidade classe 1\n");
+    }else if(imc < 39.9){
+         printf("obesidade classe 2\n");
+    }else{
+        printf("obesidade classe 3\n");
+    }
+
     return 0;
 }
