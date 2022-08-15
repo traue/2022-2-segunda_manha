99c35165e4240fc7d3acba32b9b76daee1fc8816
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-15 10:33:00 -0300

    Continuação

--- Introducao_ao_C/exercicio_fixacao_1 - IMC/main.c
+++ Introducao_ao_C/exercicio_fixacao_1 - IMC/main.c
@@ -11,35 +11,47 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main(void) {
+int main(void)
+{
 
-    //Leitura do peso
+    // Leitura do peso
     printf("Informe o peso: ");
     float peso;
     scanf("%f", &peso);
 
-    //Leitura da altura
+    // Leitura da altura
     printf("\nInforme a altura: ");
     float altura;
     scanf("%f", &altura);
 
-    //Calculo do resultado (IMC)
+    // Calculo do resultado (IMC)
     float imc = peso / (altura * altura);
 
     printf("IMC = %.2f\n", imc);
-    
-    //contribuição do Dereck (classificação):
-        if(imc < 18.5){
+
+    // contribuição do Dereck (classificação):
+    if (imc < 18.5)
+    {
         printf("abaixo do peso\n");
-    }else if( imc < 24.9){
+    }
+    else if (imc < 24.9)
+    {
         printf("peso normal\n");
-    }else if(imc < 29.9){
+    }
+    else if (imc < 29.9)
+    {
         printf("excesso de peso\n");
-    }else if(imc < 34.9){
-         printf("obesidade classe 1\n");
-    }else if(imc < 39.9){
-         printf("obesidade classe 2\n");
-    }else{
+    }
+    else if (imc < 34.9)
+    {
+        printf("obesidade classe 1\n");
+    }
+    else if (imc < 39.9)
+    {
+        printf("obesidade classe 2\n");
+    }
+    else
+    {
         printf("obesidade classe 3\n");
     }
 
